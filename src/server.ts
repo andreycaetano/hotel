0d4ed0c5d94c05225fca056ea9ab34d736b26ba2
andreycaetano <133555380+andreycaetano@users.noreply.github.com>
2024-03-02 03:35:06 -0300

    deploy

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,6 @@ import { app } from './app';
 import { main } from './database';
 
 app.listen(process.env.PORT, () => {
-    main()
     console.log(`Application started on port ${process.env.PORT} successfully!`)
+    main()
 });
