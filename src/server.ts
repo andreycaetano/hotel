648c09744cac3836fa4209abec7e0f49d0c24fdd
andreycaetano <133555380+andreycaetano@users.noreply.github.com>
2024-03-08 01:23:50 -0300

    'deploy'

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import { app } from './app';
 import { main } from './database';
 
-app.listen(() => {
-    console.log(`Application started!`)
+app.listen(process.env.PORT ,() => {
+    console.log(`Application started on port ${process.env.PORT} successfully!`)
     main()
 });
